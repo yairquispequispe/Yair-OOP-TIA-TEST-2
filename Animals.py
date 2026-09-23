05f392119d71fe8ce3ec3618cfb595a94d73c054
yairquispequispe <yiquispequispe@escuelasproa.edu.ar>
2026-09-23 12:26:46 -0300

    Refactor Animal, Vaca, and Perro classes
    Refactor Animal classes to include make_sound method and remove unnecessary parameters.

--- Animals.py
+++ Animals.py
@@ -3,19 +3,26 @@ class Animal:
         self.name = name
         self.sound = sound
 
-def make_sound(self, vaca, perro):
-    self.vaca = vaca
-    self.perro = perro
-    
+    def make_sound(self):
+        print(f"{self.name} hace: {self.sound}")
+
+
 class Vaca(Animal):
-    def __init__(self, name, sound, vaca):
+    def __init__(self, name, sound, breed):
         super().__init__(name, sound)
-        self.vaca = vaca
-        print("MUUUUU")
+        self.breed = breed
+
+
 class Perro(Animal):
-    def __init__(self, name, sound, perro):
+    def __init__(self, name, sound, breed):
         super().__init__(name, sound)
-        self.perro = perro
-        print("GUAF GUAF")
+        self.breed = breed
+
+
+vaca1 = Vaca("Lola", "MUUUUU", "Holando-Argentino")
+perro1 = Perro("Firulais", "GUAU GUAU", "Golden Retriever")
+
+vaca1.make_sound()
+perro1.make_sound()
 
     
