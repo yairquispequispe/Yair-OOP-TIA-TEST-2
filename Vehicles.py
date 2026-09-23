ac53b1266d1925943d87d07537132fbc6649dd0c
yairquispequispe <yiquispequispe@escuelasproa.edu.ar>
2026-09-23 12:27:15 -0300

    Refactor vehicle class and improve display method
    Refactored vehicle class to Vehicle with corrected display_info method and added instance creation.

--- Vehicles.py
+++ Vehicles.py
@@ -1,10 +1,13 @@
-class vehicle:
+class Vehicle:
     def __init__(self, brand, year):
         self.brand = brand
         self.year = year
-        self.Patricio = "Ford", 2007
-        self.Felipe = "Ferrari", 2022
-    
-    def dislpay_info(self):
-        print(vehicle)
-    
+        
+    def display_info(self):
+        print(f"Brand: {self.brand}, Year: {self.year}")
+
+object1 = Vehicle("Ford", 2007)
+object2 = Vehicle("Ferrari", 2011)
+
+object1.display_info()
+object2.display_info()
